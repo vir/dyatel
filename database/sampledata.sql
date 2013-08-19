3929aba6eb4b95c6ef8ce4adf8d01762fe5731fd
Vasily i. Redkin <vir@ctm.ru>
2013-08-19 16:15:09 +0400

    fix sampledata

--- database/sampledata.sql
+++ database/sampledata.sql
@@ -7,7 +7,7 @@ INSERT INTO ipnetworks(net, id) VALUES ('10.0.0/24', 1);
 INSERT INTO users (num, alias, domain, password, descr) VALUES ('222', 'vir',  'voip.ctm.ru', '222', 'vir test spa962');
 INSERT INTO users (num, alias, domain, password, descr) VALUES ('223', 'vir2', 'voip.ctm.ru', '223', 'vir test2');
 INSERT INTO users (num, domain, password, descr) SELECT s::TEXT, 'voip.ctm.ru', s::TEXT, 'test user ' || s::TEXT FROM generate_series(224, 229) AS s;
-INSERT INTO provision (uid, hw, devtype, params) VALUES (9, '000E08D48EB4', 'linksys-spa', 'number => one');
+INSERT INTO provision (uid, hw, devtype, params) VALUES ((SELECT id FROM USERS LIMIT 1), '000E08D48EB4', 'linksys-spa', 'number => one');
 
 INSERT INTO ivr_aa(num, descr, prompt, timeout, e1, e2, e3, etimeout) VALUES (
 	'222', 'Sample auto-attendant', '/home/vir/menu.au', 5,
