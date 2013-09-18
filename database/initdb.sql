62074a15a32b268de0a7b3712cfc3a62d362338f
Vasily i. Redkin <vir@ctm.ru>
2013-09-18 10:33:45 +0400

    Fix database update script

--- database/initdb.sql
+++ database/initdb.sql
@@ -438,16 +438,16 @@ ALTER TABLE cdr CLUSTER ON cdr_ts_index;
 
 -- Resource subscribtion
 --CREATE TABLE funclog (ts TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP, src VARCHAR(255) NULL, msg TEXT NOT NULL);
-CREATE OR REPLACE FUNCTION subscriptions_subscribe(notifier TEXT, operation TEXT, subscriber TEXT, data TEXT, notifyto TEXT, expires INTERVAL) RETURNS SETOF RECORD AS $$
+CREATE OR REPLACE FUNCTION subscriptions_subscribe(notifier TEXT, event_arg TEXT, subscriber TEXT, data TEXT, notifyto TEXT, expires INTERVAL) RETURNS SETOF RECORD AS $$
 BEGIN
-	-- INSERT INTO funclog(src, msg) VALUES ('subscriptions_subscribe', 'notifier=' || notifier || ', operation=' || operation || ', subscriber=' || subscriber || ', data=' || data || ', notifyto=' || notifyto || ', expires=' || expires);
-	INSERT INTO subscriptions(notifier, operation, subscriber, data, notifyto, expires) VALUES (notifier, operation, subscriber, data, notifyto, expires);
+	-- INSERT INTO funclog(src, msg) VALUES ('subscriptions_subscribe', 'notifier=' || notifier || ', operation=' || event_arg || ', subscriber=' || subscriber || ', data=' || data || ', notifyto=' || notifyto || ', expires=' || expires);
+	INSERT INTO subscriptions(notifier, operation, subscriber, data, notifyto, expires) VALUES (notifier, event_arg, subscriber, data, notifyto, expires);
 	RETURN QUERY SELECT notifier, data, subscriber, operation, notifyto, currval('subscriptions_id_seq');
 END;
 $$ LANGUAGE PlPgSQL;
 CREATE OR REPLACE FUNCTION subscriptions_unsubscribe(notifier_arg TEXT, event_arg TEXT, subscriber_arg TEXT) RETURNS VOID AS $$
 BEGIN
-	-- INSERT INTO funclog(src, msg) VALUES ('subscriptions_unsubscribe', 'notifier=' || notifier || ', operation=' || operation || ', subscriber=' || subscriber);
+	-- INSERT INTO funclog(src, msg) VALUES ('subscriptions_unsubscribe', 'notifier=' || notifier || ', operation=' || event_arg || ', subscriber=' || subscriber);
 	DELETE FROM subscriptions WHERE notifier = notifier_arg AND operation = event_arg AND subscriber = subscriber_arg;
 END;
 $$ LANGUAGE PlPgSQL;
