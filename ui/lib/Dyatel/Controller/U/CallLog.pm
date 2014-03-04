4b381f184b90fc0f1725242a7d21f05f5d3d62cf
Vasily i. Redkin <vir@ctm.ru>
2014-03-04 14:29:47 +0400

    Call log REST api added

---
+++ ui/lib/Dyatel/Controller/U/CallLog.pm
@@ -0,0 +1,86 @@
+package Dyatel::Controller::U::CallLog;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Dyatel::Controller::U::CallLog - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :PathPrefix :Chained('/') :CaptureArgs(0) { }
+
+sub call :Chained(index) :CaptureArgs(1)
+{
+	my($self, $c, $billid) = @_;
+# TODO Check access!! XXX
+	my $m = $c->model('DB::CallLog')->search({billid => $billid});
+	$c->stash(billid => $billid, m => $m);
+}
+
+sub list :Chained(call) :Args(0)
+{
+	my($self, $c) = @_;
+	$c->stash(rows => [$c->stash->{m}->search({ }, {columns => [qw/ id ts tag /], order_by => [qw/ts/]})->all]);
+}
+
+sub record :Chained(call) :Args(1)
+{
+	my($self, $c, $id) = @_;
+	my $o = ($id =~ /^\d+$/) ? $c->stash->{m}->find($id) : undef;
+	if($c->request->method eq 'POST') {
+		my %params = (
+			ts => 'now()',
+			uid => $c->stash->{uid},
+			billid => $c->stash->{billid},
+			tag => 'NOTE',
+			value => $c->request->params->{text},
+			params => undef,
+		);
+		if($o) {
+			$o->update(\%params);
+		} else {
+			$o = $c->model('DB::CallLog')->create(\%params);
+		}
+		$c->response->status(202);
+		return $c->detach;
+	} else {
+		unless($o) {
+			$c->response->status(404);
+			$c->response->body('Record not found');
+			return $c->detach;
+		}
+		$c->stash(row => $o);
+	}
+}
+
+
+=encoding utf8
+
+=head1 AUTHOR
+
+Vasily i. Redkin,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
