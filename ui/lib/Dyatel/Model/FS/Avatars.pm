ed28e1f7ea9e7f1d25e4a9f298d16d73a5b28c42
Vasily i. Redkin <vir@ctm.ru>
2014-07-03 14:46:56 +0400

    avatars directory path configuration added

--- ui/lib/Dyatel/Model/FS/Avatars.pm
+++ ui/lib/Dyatel/Model/FS/Avatars.pm
@@ -3,8 +3,23 @@ use Moose;
 use namespace::autoclean;
 use MooseX::Types::Moose qw/ArrayRef HashRef CodeRef Str ClassName/;
 extends 'Catalyst::Model';
+use Data::Dumper;
 
-has directory => (is => 'rw', isa => Str, default => sub { Dyatel->config->{root}.'/avatars' });
+# Support configuration loading even when used from external script
+my $cfg;
+eval { $cfg = Dyatel->config->{'Model::FS::Avatars'}; }; # this succeeds if running inside Catalyst
+if ($@) # otherwise if called from outside Catalyst try manual load of model configuration
+{
+	if(eval "require Dyatel::ExtConfig") {
+		$cfg = Dyatel::ExtConfig::load()->{Model}{'FS::Avatars'};
+	} else { # fallback
+		die;
+	}
+}
+__PACKAGE__->config( $cfg ); # put model parameters into main configuration
+
+has fsdir => (is => 'rw', isa => Str, default => sub { my $r = ''; $r = Dyatel->config->{root} if Dyatel->can('config') && Dyatel->config && Dyatel->config->{root}; return $r.'/avatars' });
+has webdir => (is => 'rw', isa => Str, default => '/avatars');
 
 =head1 NAME
 
@@ -29,10 +44,10 @@ sub get
 {
 	my $self = shift;
 	my($uid) = @_;
-	my $path = $self->directory . "/user_$uid.png";
-warn "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX avatar: $path XXXXXXXXXXXXXXXXXXXXXXX\n";
+	my $fn = "/user_$uid.png";
+	my $path = $self->fsdir.$fn;
 	return undef unless -f $path;
-	return "/avatars/user_$uid.png";
+	return $self->webdir.$fn;
 }
 
 __PACKAGE__->meta->make_immutable;
